5d219ced336ae5f550ffc0a1b6fbe5138dc5edd0
Kai Tamkun <ktamkun@ucsc.edu>
2021-02-06 23:26:01 -0800

    Fix a warning about array indices in _ctors_start

--- include/lib/globals.h
+++ include/lib/globals.h
@@ -1,6 +1,6 @@
 #pragma once
 
 typedef void (*ctor_t)(void);
-extern ctor_t _ctors_start[0];
-extern ctor_t _ctors_end[0];
+extern ctor_t _ctors_start[];
+extern ctor_t _ctors_end[];
 extern "C" void _init();
